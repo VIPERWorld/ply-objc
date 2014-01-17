006ff7d633c925a035fbd0e0d3df2109bf4fa33d
David Brown <dtbrown@me.com>
2014-01-17 15:04:10 -0800

    add header unit tests, first pass

--- PLYReader/PLYReaderTests/PLYReaderTests.m
+++ PLYReader/PLYReaderTests/PLYReaderTests.m
@@ -20,7 +20,7 @@
 {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    asl_add_log_file(NULL, STDERR_FILENO);
+//    asl_add_log_file(NULL, STDERR_FILENO);
     
 }
 
@@ -47,6 +47,32 @@
     XCTAssertNotNil(myPlyDictionary, @"PLYReader returned no dictionary when data was supplied!");
     
 //    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testHeader
+{
+    PLYReader *testPLYReader = nil;
+    
+    NSURL *plyUrl = [[NSBundle bundleForClass:[PLYReaderTests class]] URLForResource:@"drill_shaft_zip" withExtension:@"ply"];
+    testPLYReader = [[PLYReader alloc] initWithURL:plyUrl];
+    NSDictionary *myPlyDictionary = [testPLYReader PLYDictionary];
+
+    NSDictionary *headerDictionary = [myPlyDictionary objectForKey:kPLYReaderHeaderKey];
+    
+    XCTAssertNotNil(headerDictionary,
+                    @"PLYReader returned no header dictionary when data was supplied!");
+    
+    NSArray *commentArray = [headerDictionary objectForKey:kPLYReaderCommentKey];
+    
+    XCTAssertNotNil(commentArray, @"Comment array should have been provided.");
+    XCTAssertEqual([commentArray count], (NSUInteger)0, @"There are no comments in %@.",plyUrl);
+    
+    NSArray *elementArray = [headerDictionary objectForKey:kPLYReaderElementsKey];
+    
+    XCTAssertNotNil(elementArray, @"Element array should have been provided.");
+    XCTAssertEqual([elementArray count], (NSUInteger)2, @"There are 2 elements in %@",plyUrl);
+    
+    
 }
 
 @end
